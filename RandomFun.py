6b7e6b0e67c9c5b6cc4142df9a387c6bd1877e15
MIT PATEL <44032676+mit1280@users.noreply.github.com>
2019-09-14 22:35:07 +0530

    Add files via upload

---
+++ RandomFun.py
@@ -0,0 +1,19 @@
+import Prime as p
+import random
+def gen_Random(name,n,mode):
+    if name=="getBalancedPrime":
+        x=[]
+        while(len(x)==0):
+            start = random.randint(10**(n-1),10**n//2)
+            end = random.randint((10**n//2)+1,(10**n)-1)
+            x = getattr(p,name)(start,end,mode)
+        x=list(filter(None, x))
+        return(random.choice(x))
+    x=[]
+    while(len(x)==0):
+        start = random.randint(10**(n-1),10**n//2)
+        end = random.randint((10**n//2)+1,(10**n)-1)
+        x = getattr(p,name)(start,end)
+    x=list(filter(None, x))
+    return(random.choice(x))
+
