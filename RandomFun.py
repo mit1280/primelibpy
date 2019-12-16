8d11822611f42f752fc41d55702915da99b51b6a
MIT PATEL <44032676+mit1280@users.noreply.github.com>
2019-12-16 19:07:22 +0530

    Update RandomFun.py

--- RandomFun.py
+++ RandomFun.py
@@ -2,15 +2,24 @@ import Prime as p
 import random
 def gen_Random(name,n,mode):
     name="get"+name
+    x=[]
     if name=="getBalancedPrime":
-        x=[]
         while(len(x)==0):
             start = random.randint(10**(n-1),10**n//2)
             end = random.randint((10**n//2)+1,(10**n)-1)
             x = getattr(p,name)(start,end,mode)
         x=list(filter(None, x))
         return(random.choice(x))
-    x=[]
+    if name=="getFermatPseudoPrime":
+        if(mode<2):
+            print("enter balanced mode greater than one")
+            return x
+        while(len(x)==0):
+            start = random.randint(10**(n-1),10**n//2)
+            end = random.randint((10**n//2)+1,(10**n)-1)
+            x = getattr(p,name)(start,end,mode)
+        x=list(filter(None, x))
+        return(random.choice(x))
     while(len(x)==0):
         start = random.randint(10**(n-1),10**n//2)
         end = random.randint((10**n//2)+1,(10**n)-1)
